d739d218c74f3d16525e140a79c79f1247a64ead
Laura Tchartorijsky <lauratcharto@gmail.com>
2024-07-24 11:51:51 +0200

    add SearchBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
       <h1> I am Jammming</h1>
+      <SearchBar />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function SearchBar() {
-
+    <input id='searchBar' type='text'></input>
 };
 
 export default SearchBar;
