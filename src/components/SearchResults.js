4da1ebbd36d15041d234fe86506a4a8b5959e51d
Laura Tchartorijsky <lauratcharto@gmail.com>
2024-07-24 12:30:03 +0200

    link css modules to static components

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './SearchResults.module.css';
 
 function SearchResults() {
-    return <>
+    return <div className = {styles.maindiv}>
         <h2>Search Results</h2>
         <p>Here will be the tracklist made of tracks of the resulting search</p>
-    </>;
+    </div>;
 };
 
 export default SearchResults;
