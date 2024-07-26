6833829b00298ef109583e22279d0bbebd9137c9
Laura Tchartorijsky <lauratcharto@gmail.com>
2024-07-26 15:57:39 +0200

    add a pretend list of songs and useState for the search Results (aka songs)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import './App.css';
+import React, {useState} from 'react';
 import SearchBar from './components/SearchBar';
 import SearchResults from './components/SearchResults';
 import Playlist from './components/Playlist';
 
 function App() {
+  const [songs, setSongs] = useState([]);
+
+  const mockSongs = [
+    {
+      "title" : "First Song",
+      "artist" : "First Artist",
+      "album" : "First Album"
+    },
+    {
+      "title" : "Second Song",
+      "artist" : "Second Artist",
+      "album" : "Second Album"
+    },
+    {
+      "title" : "Third Song",
+      "artist" : "Third Artist",
+      "album" : "Third Album"
+    }
+  ];
+
+  setSongs(mockSongs);
+
   return (
     <div className='app' >
       <h1 className='h1'> I am Jammming</h1>
       <SearchBar />
-      <SearchResults />
+      <SearchResults songs={songs} />
       <Playlist />
-      <p></p>
     </div>
   );
 }
