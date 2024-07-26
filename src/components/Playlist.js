d11141e2f406b15af79220dda55fa77ea29c5731
Laura Tchartorijsky <lauratcharto@gmail.com>
2024-07-26 17:26:12 +0200

    add tracklist as an intermediate for playlist and track

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import styles from './Playlist.module.css';
-import Track from './Track';
 import Tracklist from './Tracklist';
 
 function Playlist({playlist}) {
-    console.log(playlist);
 
     return <div className = {styles.maindiv}>
         <h2>Playlist</h2>
         <p>Here will be the tracklist made up of tracks</p>
         <h3>{playlist.name}</h3>
+        <Tracklist songs={playlist.songs} />
         <button>Save to Spotify</button>
     </div>;
 };
